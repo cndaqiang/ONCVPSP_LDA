6e2b4f8e3ffce07dbe98ac297d22e43300141c51
cndaqiang <i@cndaqiang.ac.cn>
2022-04-14 13:28:28 +0800

    abinit W_sr use W.in2

--- lda.sh
+++ lda.sh
@@ -8,16 +8,18 @@
 
 rm -rf lda.out
 ONCVDIR=/home/cndaqiang/code/source/oncvpsp-4.0.1
-FUN=PBE #You still need to change awk command below.
-for i in $( ls *.in3 | grep -v lda )
+#ONCVDIR=/home/cndaqiang/code/source/oncvpsp-3.3.1
+FUN=PZ #You still need to change awk command below.
+for i in $( ls W.in2 | grep -v lda )
 do
     echo $i
     prefix=$(echo $i | awk -F. '{ print $1 }')
-    sed  3d $i | sed "3i$(sed -n  3p $i | awk '{ print $1,$2,$3,$4,4,$6 }')" > lda.$i
+    sed  3d $i | sed "3i$(sed -n  3p $i | awk '{ print $1,$2,$3,$4,3,$6 }')" > lda.$i
     ${ONCVDIR}/src/oncvpsp.x   < lda.$i > lda.out
     awk 'Begin{out=0};/END_PSP/{out=0}; {if(out == 1) {print}};\
         /Begin PSP_UPF/{out=1}' lda.out > ${prefix}_ONCV_${FUN}_sr.upf
     rm -rf lda.out
+    exit
     ${ONCVDIR}/src/oncvpspnr.x < lda.$i > lda.out
     awk 'Begin{out=0};/END_PSP/{out=0}; {if(out == 1) {print}};\
         /Begin PSP_UPF/{out=1}' lda.out > ${prefix}_ONCV_${FUN}_fr.upf
